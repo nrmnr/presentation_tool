840db5323c2350a44bc0cc567cf9ada88fb744e8
n-fujita <n-fujita@daiichi-g.co.jp>
2015-04-30 17:34:28 +0900

    refact: initialize target
    divide size, style

--- src/js/presen.js
+++ src/js/presen.js
@@ -1,11 +1,25 @@
 $(function(){
   var Presentation = function(target) {
     var $target = $(target);
-    $target.
-      css("width", "800px").
-      css("height", "600px").
-      css("border", "1px solid black").
-      css("background-color", "gray");
+
+    var init_target_size = function() {
+      $target.
+        css("width", "800px").
+        css("height", "600px");
+    };
+
+    var init_target_style = function() {
+      $target.
+        css("border", "1px solid black").
+        css("background-color", "gray");
+    };
+
+    var init_target = function() {
+      init_target_size();
+      init_target_style();
+    };
+
+    init_target();
   };
 
   new Presentation("#presen_board");
